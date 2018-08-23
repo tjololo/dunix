855673e4d865469c66c54a5c6c36839b37599a87
Vemund Gaukstad <vemundg@gmail.com>
2018-08-23 09:58:40 +0200

    updated expected value in test
    updated expected value in test to reflect changes to default

--- pkg/idea/idea_test.go
+++ pkg/idea/idea_test.go
@@ -13,7 +13,7 @@ func TestGetDownloadURI_version_defined(t *testing.T) {
 }
 
 func TestGetDownloadURI_version_not_defined(t *testing.T) {
-	expected := "https://download.jetbrains.com/idea/ideaIU-2018.1.2.tar.gz"
+	expected := "https://download.jetbrains.com/idea/ideaIU-2018.2.2.tar.gz"
 	actual := GetDownloadURI("")
 	if actual != expected {
 		t.Error("Wrong download url returned\nExpected:", expected, "\nActual: ", actual)
